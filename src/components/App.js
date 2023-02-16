c3d78945497257a95ad20f6ca8cb66afd31c1760
Suchitra-patra <suchitra3576@gmail.com>
2023-02-16 19:21:21 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,23 @@ import React, {useState} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here 
-  
+  const [value, setValue] = useState(false)
+  const handleClick = () => {
+    setValue(!value);
+    
+  }
 
   
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+        {value ? "ON" : "OFF"}
+        {/* assign value for button 1 */}
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
+        {value ? "OFF" : "ON"}
         {/* assign value for button 2 */}
       </button>
     </div>
